050e511b147a64d8f0f631709db6ae012474ba92
Emanuele <zavian.mail@gmail.com>
2021-10-21 02:15:54 +0200

    Finalized design of an xml based notecard, nothing groundbreaking, just pretty looking

---
+++ Clever Notecard/notecard.lua
@@ -0,0 +1,97 @@
+local commander = "878b50"
+
+function onload()
+    setData()
+    local data = {
+        click_function = "parse",
+        function_owner = self,
+        label = "Parse",
+        position = {1.92, 0.1, 1.33},
+        scale = {0.3, 0.3, 0.3},
+        width = 1200,
+        height = 500,
+        font_size = 400,
+        color = {0.1341, 0.1341, 0.1341, 1},
+        font_color = {1, 1, 1, 1},
+        tooltip = "Parse"
+    }
+    self.createButton(data)
+end
+
+function onHover()
+    setData()
+end
+
+function setData()
+    self.UI.setAttribute("title", "text", self.getName())
+    self.UI.setAttribute("description", "text", self.getDescription())
+end
+
+function parse()
+    if self.getDescription() ~= "" and self.getName() ~= "" then
+        local vars = JSON.decode(self.getDescription())
+        local npc_commander = getObjectFromGUID(commander)
+
+        if vars.name then
+            npc_commander.call("setName", {input = vars.name})
+        end
+
+        if vars.ini then
+            npc_commander.call("setINI", {input = vars.ini})
+        end
+
+        if vars.hp then
+            npc_commander.call("setHP", {input = vars.hp})
+        end
+
+        if vars.ac then
+            npc_commander.call("setAC", {input = vars.ac})
+        end
+
+        if vars.mov then
+            npc_commander.call("setMovement", {input = vars.mov})
+        end
+
+        if vars.size then
+            npc_commander.call("setSize", {input = vars.size})
+        end
+
+        if vars.image then
+            npc_commander.setDescription(vars.image)
+            npc_commander.call("toggleIsBoss", {input = true})
+        else
+            npc_commander.call("toggleIsBoss", {input = false})
+        end
+
+        if vars.side then
+            npc_commander.call("setSide", {input = vars.side})
+        else
+            npc_commander.call("setSide", {input = "enemy"})
+        end
+
+        if vars.extra then
+            npc_commander.call("setExtraParams", vars.extra)
+        end
+
+        if self.getGMNotes() ~= "" then
+            -- this means i have multiple that i want to make
+            local number = tonumber(self.getGMNotes())
+            npc_commander.call("setNumberToCreate", {input = number})
+        else
+            npc_commander.call("setNumberToCreate", {input = 1})
+        end
+    end
+end
+
+function mysplit(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    i = 1
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
